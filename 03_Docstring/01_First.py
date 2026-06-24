5288747a232868bd078f36d500fd8130f7a2749a
Code-Master20 <sahidurmiah201920@gmail.com>
2026-06-24 19:27:56 +0530

    documentation string

---
+++ 03_Docstring/01_First.py
@@ -0,0 +1,14 @@
+#Program to add two numbers
+def Add(x,y):
+    '''
+    This function adds two numbers.
+    It has two parameters
+    '''
+    print("Sum = ", (x+y))
+
+Add(10,15)
+
+
+# python -m pydoc -w 01_First
+# -w  --> is used to call a pydoc-module
+# -w ---> is telling that an html file to be created with 01_First.html
