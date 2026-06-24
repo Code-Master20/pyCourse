148fa19791309a502ac78fd17c3be1b6e31cc4bd
Code-Master20 <sahidurmiah201920@gmail.com>
2026-06-24 19:50:18 +0530

    None data type

---
+++ 04_DataTypes/01_None/01_First.py
@@ -0,0 +1,14 @@
+# The None Type :--
+# An object that does not contain 
+# any value is the "None", just like "null" in Java
+
+# In boolean expression None type data type reresents False
+
+x = None
+if x:
+    print("True")
+else:
+    print("False")
+
+# if x is false "else" will execute and if x is true "if" will execute
+# Result is ---> False
