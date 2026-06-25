7244c3fd9e0e99d6ebdc8864899e97b8cc683df5
Code-Master20 <sahidurmiah201920@gmail.com>
2026-06-25 20:07:11 +0530

    string-sequence data type

---
+++ 04_DataTypes/05_Sequences/01_String/01_First.py
@@ -0,0 +1,12 @@
+# String Data Type -sequence
+# String is a sequnce consist of a group of characters
+# This data type is immutable
+name = "Sahidur Miah"
+name = 'Rakesh Miah'
+para = '''Sahidur Miah is going to the market.
+he brought some sort of goods.'''
+email = """Hello sir I am Mr. Sahidur Miah.
+I need a pice of information from your side.
+Could you please give me one."""
+print(para)
+print(email)
