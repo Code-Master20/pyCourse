382d56f639da54c9d36adad09fbdf028726d8bfc
Code-Master20 <sahidurmiah201920@gmail.com>
2026-06-25 22:13:40 +0530

    utf-8 elaborated

--- 04_DataTypes/05_Sequences/02_BytesDataTypes/05_Fifth.py
+++ 04_DataTypes/05_Sequences/02_BytesDataTypes/05_Fifth.py
@@ -1,6 +1,8 @@
 # 2. Network Communication
 # When sending data over the internet, data is transmitted as bytes
 
+# UTF-8 is a character encoding standard that converts human-readable text (characters) into bytes so computers can store and transmit it.
+
 message = "Hello"
 data = message.encode("utf-8")
 print(data) # b'Hello'
@@ -13,3 +15,10 @@ for i in range(len(data)):
 # 108 -->l
 # 108 -->l
 # 111 -->o
+
+
+
+data = b'Hello'
+text = data.decode("utf-8")
+print(text) # Hello
+print(type(text)) # <class 'str'>
