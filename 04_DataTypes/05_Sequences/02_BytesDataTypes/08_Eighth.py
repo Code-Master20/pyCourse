64f90aa757f785ae014c53f992a2e07826733e70
Code-Master20 <sahidurmiah201920@gmail.com>
2026-06-26 10:07:19 +0530

    how to use dir to see available mehods for a specific object

--- 04_DataTypes/05_Sequences/02_BytesDataTypes/08_Eighth.py
+++ 04_DataTypes/05_Sequences/02_BytesDataTypes/08_Eighth.py
@@ -46,3 +46,17 @@ print(hashlib.algorithms_available)
 
 # digest()
 # Returns raw bytes.
+
+
+# to see available methods on a specific object
+# dir(object)
+print(dir(hash_value))
+# Result -->
+# ['__class__', '__delattr__', '__dir__', '__doc__', 
+#  '__eq__', '__format__', '__ge__', '__getattribute__',
+#  '__getstate__', '__gt__', '__hash__', '__init__', 
+#  '__init_subclass__', '__le__', '__lt__', '__module__', 
+#  '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', 
+#  '__setattr__', '__sizeof__', '__str__', '__subclasshook__', 
+#  'block_size', 'copy', 'digest', 'digest_size', 'hexdigest', 
+#  'name', 'update']
